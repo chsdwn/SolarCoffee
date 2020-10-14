da06941eb2efd57004e72bdaa3fb1ebc03cdaa1d
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-14 13:00:00 +0300

    Bug Fix: CORS and Sending Inventory Data

--- solarcoffee.frontend/src/services/inventory-service.ts
+++ solarcoffee.frontend/src/services/inventory-service.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import { IProductInventory } from '@/types/Product';
 
 export class InventoryService {
   API_URL = process.env.VUE_APP_API_URL;
 
-  public async getInventory(): Promise<any> {
-    const result = axios.get(`${this.API_URL}/inventory`);
-    console.log(result);
+  public async getInventory(): Promise<IProductInventory[]> {
+    const result = await axios.get<IProductInventory[]>(
+      `${this.API_URL}/inventory`,
+    );
+    return result.data;
   }
 }
